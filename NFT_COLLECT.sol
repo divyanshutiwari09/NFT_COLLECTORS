4b56ee9a8bd93f227b5e4e7c46930c6369f730ab
divyanshutiwari09 <133540138+divyanshutiwari09@users.noreply.github.com>
2023-05-28 23:34:47 +0530

    Update NFT_COLLECT.sol

--- NFT_COLLECT.sol
+++ NFT_COLLECT.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+
+contract DIVYANSHU_COIN is ERC20 {
+   constructor(string memory _DIVYANSHU_COIN, string memory _DTC) ERC20(_DIVYANSHU_COIN, _DTC) 
+   {
+_mint(msg.sender, 10 * 10 ** 18);
+}
+}
